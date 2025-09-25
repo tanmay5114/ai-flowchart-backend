03f6f771f6c6ab40cef58dc1a88d10c52c616370
Tanmay Jain <tanmayjain4477@gmail.com>
2025-09-25 17:32:46 +0530

    removed unnecessary zod schema

--- src/types/zodSchema.ts
+++
@@ -1,6 +0,0 @@
-import z from "zod"
-
-export const addThreadSchema = z.object({
-    content: z.string().min(3, "Thread length should be of minimum 3 characters").optional(),
-    tags: z.string().min(1).optional(), // need to send tag ids from the frontend in this manner -> "1, 2, 4, 5, 2, 2, 1"
-})
